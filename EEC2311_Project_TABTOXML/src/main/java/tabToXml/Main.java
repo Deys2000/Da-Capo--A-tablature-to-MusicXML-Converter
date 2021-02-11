be6d194a06c1b2a482bd37d4e2faaa070b224815
syed2000 <ameenquadri2000@gmail.com>
2021-02-11 17:15:03 -0500

    Updating JavaFX library from version 11 to 15

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
@@ -2,7 +2,6 @@ package tabToXml;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -19,7 +18,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 	
-		System.out.print("Launching Application");
+		System.out.println("Launching Application");
 		launch(args);
 		//new stuff
 		
