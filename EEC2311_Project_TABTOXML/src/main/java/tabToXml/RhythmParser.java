de86a572cca9377f407e97f92492bb1ca9aecfc4
Gong-Fan Bao <billy.bao11@gmail.com>
2021-02-26 18:36:21 -0500

    rhythm parser now works for chords, added comments for readability

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/RhythmParser.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/RhythmParser.java
@@ -9,7 +9,7 @@ public class RhythmParser {
     
     private int divisions = 0;
     private int padding = 1;
-
+     
     public RhythmParser(int divisions) { // divisions should be 4 by default
         durationArr = new ArrayList<String>();
         typeArr = new ArrayList<String>();
@@ -18,37 +18,41 @@ public class RhythmParser {
     
     /**
      * Generates duration and type arrays from parsed array
-     * @param
-     * @throws Exception
+     * @param parsedTab - a formatted tab (arrayList of Strings)
      */
     public void parseToRhythm(ArrayList<String> parsedTab) {
+    	
+    	// For Debugging
+    	for(int i = 0; i < parsedTab.size(); i++) {
+    		System.out.println(parsedTab.get(i));
+    	}
         
-        int counter = 0;
+        int counter = 0; // iterates horizontally
         int noteLength = 0; // in 16th notes
-        int lines = parsedTab.size();
+        int lines = parsedTab.size(); // number of lines in staff
         int currentLine = 0;
-        //int prevChordNum = 0;
-        //int curChordNum = 0;
-        //String rhythmLine = "";
-        
-        boolean hasFret = false;
-        boolean isCounting = false;
+        int prevChordNum = 0; // number of notes in previous chord
+        int curChordNum = 0; // number of notes in current chord
+
+        boolean isCounting = false; // if we're currently counting noteLength
         
         while (counter < parsedTab.get(0).length() - 2) { // Changed from -1 to -2, the parsedTab is one space longer than expected ???
             
             currentLine = 0;
-            hasFret = false;
             
             // Skip "|" and padding "-"
             if (parsedTab.get(0).charAt(counter) == '|') {
                 
                 // Assuming note lengths end at barlines
                 if(noteLength != 0) {
-                	//while (prevChordNum > 0) {
+                	// Add all tracked notes to arrays
+                	while (prevChordNum > 0) {
 	                    durationArr.add("" + noteLength);
 	                    typeArr.add(durationToType(noteLength, divisions));
-	                    //prevChordNum--;
-                	//}
+	                    prevChordNum--;
+                	}
+                	
+                	// since we have reached end of measure, reset and stop counting noteLength
                     noteLength = 0;
                     isCounting = false;
                 }
@@ -58,69 +62,71 @@ public class RhythmParser {
                 counter += padding; // skipping both '|' and padding '-'
             }
             
-            // Should be only run before a note is encountered
+            // Should be run before encountering the first note/chord in a measure
             else if (isCounting == false) {
                 
-                // Check when the next note starts // NOTE ONLY WORKS FOR FRETS 0-9
-                while(hasFret == false && currentLine < lines) {
+                // Check when the next note starts 
+                while(currentLine < lines) {
                     
+                	// Add all notes to a chord
                     if(Character.isDigit(parsedTab.get(currentLine).charAt(counter))) {
-                        isCounting = true;
-                        noteLength++;
-                        //prevChordNum++;
-                        
-                        hasFret = true;
+                    	
+                    	// should only be done once
+                    	if (isCounting == false) {
+                    		isCounting = true;
+                    		noteLength++;
+                    	}
+                    	
+                        prevChordNum++;
                     }
                     
                     currentLine++;
                 }
             }
             
-            // Should be run all other times
+            // Should be run after encountering the first note/chord in a measure
             else if (isCounting == true) {
                 
-                while(hasFret == false && currentLine < lines) {
+                while(currentLine < lines) {
                     
+                	// Adds all previous chord notes to arrays, then adds current notes to current chord
                     if(Character.isDigit(parsedTab.get(currentLine).charAt(counter))) {
-                    	//while (prevChordNum > 0) {
+                    	while (prevChordNum > 0) {
 	                        durationArr.add("" + noteLength);
 	                        typeArr.add(durationToType(noteLength, divisions));
-	                        //prevChordNum--;
-                    	//}
+	                        prevChordNum--;
+                    	}
+                    	
+                    	// reset note Length
                         noteLength = 0;
-                        //curChordNum++;
-                        hasFret = true;
+                        curChordNum++;
                     }
                     
                     currentLine++;
                 }
                 
-                //prevChordNum = curChordNum;
-                //curChordNum = 0;
+                // if there are notes in the current chord, copy them to previous chord and empty current chord
+                if(curChordNum != 0) {
+                	prevChordNum = curChordNum;
+                	curChordNum = 0;
+                }
+                
                 noteLength++;
             }
             
             counter++;
         }
         
-        // Last note length and ending barline is added
-        //while (prevChordNum > 0) {
+        // Last chord/note length and ending barline is added
+        while (prevChordNum > 0) {
 	        durationArr.add("" + noteLength);
 	        typeArr.add(durationToType(noteLength, divisions));
-	        //prevChordNum--;
-        //}
+	        prevChordNum--;
+        }
         durationArr.add("||");
         typeArr.add("||");
         
-        // small temporary fix for error- Syed
-        
-//        if(durationArr.size() > 3 && typeArr.size() > 3) {
-//        	durationArr.remove(durationArr.size()-2);
-//        	durationArr.remove(durationArr.size()-2);
-//        	typeArr.remove(typeArr.size()-2);
-//        	typeArr.remove(typeArr.size()-2);
-//        }
-        
+        // For debugging
         System.out.print("Duration Array: ");
         System.out.println(durationArr);
         System.out.print("Type Array: ");
@@ -152,7 +158,6 @@ public class RhythmParser {
         }
         
         return result;
-        
         
     }    
     public ArrayList<String> getTypeArr(){
