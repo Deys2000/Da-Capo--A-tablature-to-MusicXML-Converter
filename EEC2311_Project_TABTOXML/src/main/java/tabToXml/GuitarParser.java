9e54042e67b53b1af2be5ca47764da64d722a16c
Elijah Nnorom <elyjannzira@gmail.com>
2021-02-27 20:28:30 -0500

    New class called Bass Guiter

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/GuitarParser.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/GuitarParser.java
@@ -24,14 +24,14 @@ public class GuitarParser {
 	/*This method takes the input file and parses that into a 2d array. 
 	 *The result of that 2d array is used to obtain the fret and chord/note
 	 *using the translate() method above. */
-	public void translateParsed(String inputfile) throws Exception {
+	public void translateParsed(ArrayList<String> parsedTab) throws Exception {
 		
-		TextFileReader tabReader = new TextFileReader(inputfile);
+		//TextFileReader tabReader = new TextFileReader(inputfile);
 		
-		int row = tabReader.getParsed().size();
+		int row = parsedTab.size();
 //		int col = tabReader.printParsed().get(0).size();
 
-		int next = tabReader.getParsed().get(0).length();
+		int next = parsedTab.get(0).length();
 
 		char fret2 = '\0';
 		String tmp = "";
@@ -49,7 +49,7 @@ public class GuitarParser {
 					
 					String fretStringVar = "";
 					
-					fret2 = tabReader.getParsed().get(i).charAt(j);
+					fret2 = parsedTab.get(i).charAt(j);
 
 					int fret = Character.getNumericValue(fret2);
 					if (fret2 >= '0' && fret2 <= '9') {
@@ -400,7 +400,7 @@ public class GuitarParser {
             result = "eighth";
         }
         else if (durOverDiv == 0.25) {
-            result = "sixteenth";
+            result = "16th";
         }
         else {
         	result = "unidentified";
