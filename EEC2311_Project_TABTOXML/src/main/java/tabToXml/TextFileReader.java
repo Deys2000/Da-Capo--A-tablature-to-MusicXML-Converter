d42db7851905e39f5ba2b3e7b363d4fe4682ce15
miguel mesa <mesa22@my.yorku.ca>
2021-02-28 22:56:00 -0500

    some touches to textfilereader

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/TextFileReader.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/TextFileReader.java
@@ -16,6 +16,7 @@ public class TextFileReader {
 	static int numOfLines = 0;
 	boolean isDrum;
 	static String instrument;
+	static String lineStorage;
 	
 	//Parsed text 
 	public ArrayList<String> parsedTab = new ArrayList<String>();
@@ -45,9 +46,27 @@ public class TextFileReader {
 	private void countLines(){
 		Scanner sc = null;
 		try {
+			int counter = 0;
+			int loopcheck = -1;
 			sc = new Scanner(inputFile);
-			while(sc.hasNextLine()){	
+			while(sc.hasNextLine()){			
+				//for line counting
 				String next = sc.nextLine();
+				
+				//getting array for dash counting for time signature
+				//not done
+				char [] info  = next.toCharArray();
+				for(int i = 2; i < info.length; i++) {
+					counter ++;
+					if(loopcheck < 1 && info[i] == '|') {
+						loopcheck++;
+						int trueCount = counter - 1;
+						System.out.println(trueCount);
+					}
+					
+				}
+				//end of dash counting
+				
 				//drum tab check
 				if(next.contains("X") || next.contains("x") || next.contains("O") || next.contains("o"))  {
 					isDrum = true;
@@ -56,7 +75,6 @@ public class TextFileReader {
 				
 				if (next.contains("-") && next.contains("|")) {
 					numOfLines ++;
-					System.out.println(numOfLines);
 				}
 				else if( 0 < numOfLines )  // modified this line to cater to prevent crashing with spacing at the start
 					break;
@@ -131,6 +149,30 @@ public class TextFileReader {
 			sc.close();
 		}
 	}
+	//checks number of dashes in first line
+//	public void checkDashes() {
+//		Scanner sc = null;
+//		try {
+//			
+//			sc = new Scanner(inputFile);
+//			lineStorage = sc.nextLine();
+//			
+//			
+//			char [] info  = lineStorage.toCharArray();
+//			for(int i = 0; i < info.length; i++) {
+//				System.out.println(info[i]);
+//			}		
+//		}
+//		
+//		catch(FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		finally {
+//			sc.close();
+//		}
+//		System.out.println(lineStorage);
+//	}
 	/**
 	 * determines the instrument based on the number of lines
 	 * still in progress
@@ -147,7 +189,6 @@ public class TextFileReader {
 		else{
 			instrument = "Drums";
 		}
-		//for now we just return guitar
 		return instrument;
 	}
 	
@@ -158,8 +199,6 @@ public class TextFileReader {
 	public static java.lang.String staffLines(){
 		Integer count = numOfLines/2;
 		java.lang.String lines = count.toString();
-		
-		
 		return lines;
 	}
 	
