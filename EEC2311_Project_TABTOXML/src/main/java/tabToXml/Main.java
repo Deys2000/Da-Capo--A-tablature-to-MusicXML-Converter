042943ffa68d4ea5e2129c4068fd0a4930bce0c2
Ayub <aosman30@my.yorku.ca>
2021-03-22 01:27:39 -0400

    updated ui- new layout, Drag and drop works, xml code display works. still need to add save button and add edit tab feature.

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
 		primaryStage.setTitle("Da Capo : Text Tab to MusicXML Converter - version 1.0");
 		// keep scene equal or bigger than default in fxml - 800x500
-	    primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Home3.fxml")), 832,679 ));
+	    primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Home3.fxml"))));
 	    primaryStage.getIcons().add(new Image("file:DaCapo.png"));
 		primaryStage.getScene().getStylesheets().add(test.class.getResource("xml-highlighting.css").toExternalForm());
 		primaryStage.initStyle(StageStyle.UNDECORATED);
