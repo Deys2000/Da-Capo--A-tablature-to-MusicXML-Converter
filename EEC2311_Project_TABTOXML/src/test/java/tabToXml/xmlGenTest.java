8ae01156bb842f3d76b772b540d7cf49627d28a6
syed2000 <ameenquadri2000@gmail.com>
2021-04-13 12:41:45 -0400

    added elijahs test

--- EEC2311_Project_TABTOXML/src/test/java/tabToXml/xmlGenTest.java
+++ EEC2311_Project_TABTOXML/src/test/java/tabToXml/xmlGenTest.java
@@ -2,6 +2,8 @@ package tabToXml;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class xmlGenTest {
@@ -42,7 +44,22 @@ class xmlGenTest {
 //	void test_CreateGraceNote() throws Exception{
 //		
 //	}
-	
+	@Test
+	void testGuiterGenerator() throws Exception {
+		ArrayList<String> test = new ArrayList<>();
+		test.add("E|-----------------|");
+		test.add("B|-----------------|");
+		test.add("G|-----------------|");
+		test.add("D|-----------------|");
+		test.add("A|-----------------|");
+		test.add("E|-2---------------|");
+
+		GuitarParser guitarParser = new GuitarParser(test);
+		xmlGen xml = new xmlGen(guitarParser);
+		xml.getXMLContent();
+		System.out.println("In xmlGne Tester: \t" + xml);
+		assertEquals(xml, "Expected");
+	}
 	
 	
 	
